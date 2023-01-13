874ac7346a7bb9e9bed579cde76968a5b338b81e
Oscar Rolf <95602879+oscareng@users.noreply.github.com>
2023-01-13 01:25:37 -0800

    Update discord.py

--- discord.py
+++ discord.py
@@ -1,8 +1,8 @@
 import requests
 
 # URL of the webhook
-WEBHOOK_URL_REVIEW = "https://discord.com/api/webhooks/1061741992448626729/xIbdIN2vJq7Ei99aMNYr-V5hYMUOwV6jeETR2sbzXOxssVhBFdE6bfg9WAx_kflU9u3t"
-WEBHOOK_URL_APPROVED = "https://discord.com/api/webhooks/1061742548202311741/oqeg0KGQhUFeYXXEaSVBYEXSVP00t137ZZMJxMYdBpr6BhGW70r6sbgY-3ho6ShmmMKg"
+WEBHOOK_URL_REVIEW = 
+WEBHOOK_URL_APPROVED =
 # Function to send an embedded message with a hyperlink to the Discord channel
 def send_embedded_message(title, description, url, color, fields):
     # Create the embed object
